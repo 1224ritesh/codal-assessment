1d048f8f6a46034395a3f9a811fadfcece572132
Ritesh Sharma <98305600+1224ritesh@users.noreply.github.com>
2023-06-30 19:41:52 +0530

    assessment

---
+++ codal/src/screens/Homepage/Homepage.jsx
@@ -0,0 +1,319 @@
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <>
+      <div className="homepage">
+        <div className="div">
+          <div className="overlap">
+            <div className="text-wrapper">For Employers</div>
+          </div>
+          <div className="overlap-group">
+            <div className="text-wrapper-2">For Job seekers</div>
+          </div>
+          <div className="overlap-2">
+            <h1 className="h-1">
+              Expertly matching digital talent with business
+            </h1>
+            <p className="p">
+              We promote innovation, growth, and success in the digital sphere
+              by connecting businesses with the best digital talent.
+            </p>
+            <div className="frame">
+              <div className="text-wrapper-3">SEARCH JOBS</div>
+            </div>
+            <div className="div-wrapper">
+              <div className="text-wrapper-3">REQUEST TALENT</div>
+            </div>
+            <img
+              className="pexels-fauxels"
+              alt="Pexels fauxels"
+              src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/pexels-fauxels-3182812-removebg-1.png"
+            />
+          </div>
+          <div className="group">
+            <div className="text-wrapper-4">CODAL</div>
+            <div className="frame-2">
+              <div className="group-2">
+                <div className="text-wrapper-5">Employer</div>
+              </div>
+              <div className="group-3">
+                <div className="text-wrapper-5">Job seeker</div>
+              </div>
+              <div className="text-wrapper-6">Blog</div>
+              <div className="group-4">
+                <div className="text-wrapper-5">About Us</div>
+              </div>
+              <div className="frame-wrapper">
+                <div className="frame-3">
+                  <div className="text-wrapper-7">Contact Us</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="vector"
+            alt="Vector"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/vector.svg"
+          />
+          <img
+            className="img"
+            alt="Vector"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/vector-1.svg"
+          />
+          <img
+            className="group-5"
+            alt="Group"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/group-326@2x.png"
+          />
+          <div className="text-wrapper-8">CODAL</div>
+          <div className="text-wrapper-9">Let’s craft brilliance together!</div>
+          <div className="group-6">
+            <div className="frame-4">
+              <div className="text-wrapper-10">Get in touch</div>
+            </div>
+            <div className="input-filds-wrapper">
+              <div className="input-filds">
+                <div className="text-wrapper-11">Enter your Email address</div>
+              </div>
+            </div>
+          </div>
+          <div className="text-wrapper-12">Contact</div>
+          <p className="text-wrapper-13">2023 Codal. All rights reserved</p>
+          <div className="text-wrapper-14">Terms and conditions</div>
+          <div className="telephone">Telephone:&nbsp;&nbsp;+91 928282828</div>
+          <div className="email-hello-codal">
+            Email:&nbsp;&nbsp;hello.codal@gmail.com
+          </div>
+          <p className="text-wrapper-15">Advantages of working with us</p>
+          <img
+            className="line"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/line-5.svg"
+          />
+          <img
+            className="line-2"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/line-7.svg"
+          />
+          <div className="text-wrapper-16">Skilled Talent</div>
+          <div className="text-wrapper-17">Time Saving solutions</div>
+          <div className="text-wrapper-18">Cost effective results</div>
+          <p className="text-wrapper-19">
+            We have candidates who can meet any requirement. Hiring qualified
+            candidates is a difficult business challenge. We collaborate with
+            you to swiftly, efficiently, and successfully attract the right
+            professionals to your company.
+          </p>
+          <p className="say-goodbye-to">
+            Say goodbye to endless hours of searching and interviewing. Say
+            goodbye to time-wasting hiring processes. Experience a stress-free
+            and efficient talent recruitment process today.&#34;
+          </p>
+          <p className="unlock-savings-and">
+            Unlock savings and maximize ROI. Our flexible solutions provide
+            greater cost optimization. Choose us for cost-effective and
+            quality-driven talent acquisition that drives your success and saves
+            you money.&#34;
+          </p>
+          <div className="text-wrapper-20">Our Services</div>
+          <img
+            className="line-3"
+            alt="Line"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/line-13@2x.png"
+          />
+          <div className="overlap-3">
+            <div className="frame-5">
+              <div className="overlap-4">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group-2">
+                    <p className="amet-minim-mollit">
+                      Amet minim mollit non deserunt ullamco est sit aliqua
+                      dolor do amet sint. Velit officia consequat duis enim
+                      velit mollit. Exercitation veniam consequat sunt nostrud
+                      amet.&nbsp;&nbsp;Velit officia consequat duis enim velit
+                      mollit. Exercitation veniam consequat sunt nostrud amet.
+                    </p>
+                    <div className="text-wrapper-21">Amy Smith</div>
+                    <img
+                      className="rectangle"
+                      alt="Rectangle"
+                      src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/rectangle-5-2@2x.png"
+                    />
+                  </div>
+                </div>
+                <div className="text-wrapper-22">
+                  Software engineer, Banglore
+                </div>
+              </div>
+              <div className="overlap-5">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group-2">
+                    <p className="amet-minim-mollit">
+                      Amet minim mollit non deserunt ullamco est sit aliqua
+                      dolor do amet sint. Velit officia consequat duis enim
+                      velit mollit. Exercitation veniam consequat sunt nostrud
+                      amet.&nbsp;&nbsp;Velit officia consequat duis enim velit
+                      mollit. Exercitation veniam consequat sunt nostrud amet.
+                    </p>
+                    <div className="text-wrapper-21">Amy Smith</div>
+                    <img
+                      className="rectangle"
+                      alt="Rectangle"
+                      src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/rectangle-5-2@2x.png"
+                    />
+                  </div>
+                </div>
+                <div className="text-wrapper-22">
+                  Software engineer, Banglore
+                </div>
+              </div>
+              <div className="overlap-6">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group-2">
+                    <p className="amet-minim-mollit">
+                      Amet minim mollit non deserunt ullamco est sit aliqua
+                      dolor do amet sint. Velit officia consequat duis enim
+                      velit mollit. Exercitation veniam consequat sunt nostrud
+                      amet.&nbsp;&nbsp;Velit officia consequat duis enim velit
+                      mollit. Exercitation veniam consequat sunt nostrud amet.
+                    </p>
+                    <div className="text-wrapper-21">Amy Smith</div>
+                    <img
+                      className="rectangle"
+                      alt="Rectangle"
+                      src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/rectangle-5-2@2x.png"
+                    />
+                  </div>
+                </div>
+                <div className="text-wrapper-22">
+                  Software engineer, Banglore
+                </div>
+              </div>
+            </div>
+            <div className="text-wrapper-23">What our customers say</div>
+            <img
+              className="line-4"
+              alt="Line"
+              src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/line-7.svg"
+            />
+          </div>
+          <div className="text-wrapper-24">02</div>
+          <div className="text-wrapper-25">03</div>
+          <div className="text-wrapper-26">01</div>
+          <div className="overlap-7">
+            <div className="group-7">
+              <img
+                className="blob"
+                alt="Blob"
+                src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/blob-2.png"
+              />
+              <div className="text-wrapper-27">Marketing</div>
+              <p className="text-wrapper-28">
+                Grow your business with our marketing services! We offer the
+                tools and expertise you need to make your brand stand out in a
+                crowded market. Our experienced team is dedicated to helping you
+                develop a customized marketing plan tailored to your unique
+                goals and objectives. With our help, you can reach your target
+                audience and maximize your business success.
+              </p>
+            </div>
+            <img
+              className="christina"
+              alt="Christina"
+              src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/christina-wocintechchat-com-vpcgtekereq-unsplash--1--removebg-pr@2x.png"
+            />
+          </div>
+          <div className="overlap-8">
+            <div className="group-8">
+              <img
+                className="blob-2"
+                alt="Blob"
+                src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/blob-3.png"
+              />
+              <div className="text-wrapper-29">Management consulting</div>
+              <p className="text-wrapper-30">
+                Need help managing your business? Look no further than our
+                management consulting services. Our experienced professionals
+                are here to help you develop cost-effective strategies and
+                solutions to maximize profits and minimize losses. With our
+                help, you can improve operations, enhance customer service and
+                increase efficiency. Let us help you make your business a
+                success.
+              </p>
+            </div>
+            <img
+              className="christina-2"
+              alt="Christina"
+              src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/christina-wocintechchat-com-swi1dgrcshq-unsplash-removebg-previe@2x.png"
+            />
+          </div>
+          <div className="overlap-9">
+            <div className="group-9">
+              <img
+                className="blob-2"
+                alt="Blob"
+                src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/blob-2.png"
+              />
+              <div className="text-wrapper-31">Application Development</div>
+              <p className="text-wrapper-32">
+                Are you looking for a reliable application development service
+                that will help you stay ahead of the competition? Look no
+                further! From web-based applications to mobile ones, our team is
+                dedicated to delivering the best possible outcome for your
+                project. We guarantee that you will be satisfied with the
+                results. Contact us today and get started on your application
+                development project!
+              </p>
+            </div>
+            <img
+              className="app-removebg-preview"
+              alt="App removebg preview"
+              src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/app-removebg-preview-1@2x.png"
+            />
+          </div>
+          <div className="overlap-10">
+            <div className="group-9">
+              <img
+                className="blob"
+                alt="Blob"
+                src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/blob-3.png"
+              />
+              <div className="text-wrapper-33">User experience design</div>
+              <p className="text-wrapper-34">
+                Designing a great user experience is essential for any
+                successful website. Let us take the hassle for you. Our team of
+                UX designers can create an intuitive and responsive design to
+                make sure your website delivers a great user experience. We will
+                ensure your website is effective in meeting your business goals.
+                Let us help you create a better user experience today.
+              </p>
+            </div>
+            <img
+              className="disruptivo"
+              alt="Disruptivo"
+              src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/disruptivo-iq15dtx-63k-unsplash-removebg-preview-1@2x.png"
+            />
+          </div>
+          <img
+            className="dot-seamless-pattern"
+            alt="Dot seamless pattern"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/dot-seamless-pattern-1.svg"
+          />
+          <img
+            className="dot-seamless-pattern-2"
+            alt="Dot seamless pattern"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/dot-seamless-pattern-2.svg"
+          />
+          <img
+            className="dot-seamless-pattern-3"
+            alt="Dot seamless pattern"
+            src="https://generation-sessions.s3.amazonaws.com/0aa59e8c78f0e4ddd7822e65b9f8c3a2/img/dot-seamless-pattern-3.svg"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
